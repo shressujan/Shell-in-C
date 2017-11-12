b84edd129f84814f4a906b365a0002872873e9b1
Sujan Shrestha <shrestha94.sujan@gmail.com>
2017-11-12 10:26:19 -0600

    Initial Commit

---
+++ project2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project2
+//
+//  Created by Sujan Shrestha on 11/12/17.
+//  Copyright © 2017 Sujan Shrestha. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
